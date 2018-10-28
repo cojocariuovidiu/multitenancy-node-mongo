1fe7a89705d5b923a367287245e92ebf16bfc803
Aman Mahalgavan <Aman.Mahalgavan@ap.jll.com>
2018-10-28 23:07:51 +0530

    added api for registering new tenants. This creates a new database for the new tenant and inserts a sample master user into the new DB. The Tenant info is added to the master DB

--- controllers/base_controller.js
+++ controllers/base_controller.js
@@ -13,4 +13,28 @@ module.exports = {
             return res.status(200).json({ data: tenants });
         });
     },
+    register(req, res) {
+        if (!req.body.name) {
+            return res.status(400).json({ Error: "Please provide proper DB connection information and Tenant Name." });
+        }
+        let tenant = {
+            name: req.body.name,
+            connection: {
+                domain: req.body.connection.domain ? req.body.connection.domain : "mongodb",
+                host: req.body.connection.host ? req.body.connection.host : 'localhost',
+                port: req.body.connection.port ? req.body.connection.port : '27017',
+                db: req.body.connection.db ? req.body.connection.db : randomString.generate()
+            }
+        }
+        Tenant.create(tenant, (err, doc) => {
+            if (err) return res.status(500).json({ "Error": err });
+            console.log("New DB Conneection --> ", doc);
+            User(doc.connection).create({ name: "master" }, (error, user) => {
+                if (error) {
+                    return res.status(500).json({ Error: "Unable to insert master user in the new tenant database." });
+                }
+                return res.status(200).json({ message: "New Tenant created and master user inserted into the new tenant database" });
+            });
+        });
+    },
 };
--- routes/routes.js
+++ routes/routes.js
@@ -2,5 +2,6 @@ const BaseController = require('../controllers/base_controller');
 
 module.exports = (app) => {
     app.get('/fetchAllTenants', BaseController.fetchAllTenants);
+    app.post('/register', BaseController.register);
 
 };
